5ff697023591ca9494292fc36a4dbe511f375189
Sergey Rusakov <sergeyrusakov1@yandex.ru>
2020-04-13 15:20:59 +0500

    test commit from intellij

--- backend/src/main/java/ru/sergeyrusakov/testingTask/controllers/UserController.java
+++ backend/src/main/java/ru/sergeyrusakov/testingTask/controllers/UserController.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-//@CrossOrigin(origins = "http://localhost:4000")
 public class UserController {
 
     private UserRepository repository;
